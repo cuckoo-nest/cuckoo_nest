a598c9cf1b6e97a253ed4142f222516b0b68a0e7
alex <alex@the-browns.gb.net>
2025-12-06 17:38:43 +0000

    Add script to build arm

---
+++ build_arm.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+mkdir -p ./build_arm
+cmake -B ./build_arm -DCMAKE_TOOLCHAIN_FILE=../cmake/arm-toolchain.cmake
+cmake --build ./build_arm/ --target lvgl
+cmake --build ./build_arm/ --target cuckoo
