636bdd1adf99882bddb7079361c37a73a9c56dc9
alex <alex@the-browns.gb.net>
2025-12-14 19:36:40 +0000

    reinstate tests with stubs for screens

---
+++ tests/ScreenStubs/AnalogClockScreen.cpp
@@ -0,0 +1,18 @@
+#include "AnalogClockScreen.hpp"
+
+AnalogClockScreen::AnalogClockScreen(HAL *hal,
+        ScreenManager* screenManager)  
+{
+}
+
+AnalogClockScreen::~AnalogClockScreen()
+{
+}
+
+void AnalogClockScreen::Render()
+{
+}
+
+void AnalogClockScreen::handle_input_event(const InputDeviceType device_type, const struct input_event &event)
+{
+}
---
+++ tests/ScreenStubs/HomeScreen.cpp
@@ -0,0 +1,9 @@
+#include "HomeScreen.hpp"
+
+void HomeScreen::Render()
+{
+}
+
+void HomeScreen::handle_input_event(const InputDeviceType device_type, const struct input_event &event)
+{
+}
