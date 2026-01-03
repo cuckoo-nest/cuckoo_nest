e26c6e0790bbe2302955580ae72034046b1e2e56
alex <alex@the-browns.gb.net>
2026-01-03 18:37:24 +0000

    Test build with the correct toolchain

--- cmake/arm-toolchain.cmake
+++ cmake/arm-toolchain.cmake
@@ -4,69 +4,54 @@
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR arm)
 
-# Linaro Toolchain settings
-set(LINARO_VERSION "gcc-linaro-4.9.4-2017.01-x86_64_arm-linux-gnueabi")
-set(LINARO_URL "https://releases.linaro.org/components/toolchain/binaries/4.9-2017.01/arm-linux-gnueabi/${LINARO_VERSION}.tar.xz")
-set(LINARO_DIR "${CMAKE_BINARY_DIR}/linaro_toolchain")
+# ARM Toolchain settings
+# Using Linaro arm-linux-gnueabihf 4.8-2014.04 toolchain
+set(LINARO_DIR "/home/alex/gcc-linaro-arm-linux-gnueabihf-4.8-2014.04_linux")
 set(LINARO_BIN "${LINARO_DIR}/bin")
-set(LINARO_TARBALL "${CMAKE_BINARY_DIR}/${LINARO_VERSION}.tar.xz")
+set(LINARO_SYSROOT "${LINARO_DIR}/arm-linux-gnueabihf/libc")
 
 # Device sysroot repository
 set(DEVICE_SYSROOT_REPO "https://github.com/cuckoo-nest/sysroot.git")
-set(DEVICE_SYSROOT "${CMAKE_BINARY_DIR}/device_sysroot")
+#set(DEVICE_SYSROOT "${CMAKE_BINARY_DIR}/device_sysroot")
+set(DEVICE_SYSROOT "/home/alex/nest-sysroot")
 
-# Download and extract toolchain if not already present
-if(NOT EXISTS "${LINARO_BIN}/arm-linux-gnueabi-gcc")
-    message(STATUS "Linaro toolchain not found, downloading...")
-    
-    if(NOT EXISTS "${LINARO_TARBALL}")
-        file(DOWNLOAD "${LINARO_URL}" "${LINARO_TARBALL}"
-             SHOW_PROGRESS
-             STATUS DOWNLOAD_STATUS)
-        list(GET DOWNLOAD_STATUS 0 STATUS_CODE)
-        if(NOT STATUS_CODE EQUAL 0)
-            message(FATAL_ERROR "Failed to download Linaro toolchain from ${LINARO_URL}")
-        endif()
-    endif()
-    
-    message(STATUS "Extracting Linaro toolchain...")
-    file(ARCHIVE_EXTRACT INPUT "${LINARO_TARBALL}" DESTINATION "${CMAKE_BINARY_DIR}")
-    file(RENAME "${CMAKE_BINARY_DIR}/${LINARO_VERSION}" "${LINARO_DIR}")
-    message(STATUS "Linaro toolchain extracted to ${LINARO_DIR}")
-endif()
+# # Verify toolchain exists
+# if(NOT EXISTS "${LINARO_BIN}/arm-none-linux-gnueabi-gcc")
+#     message(FATAL_ERROR "ARM toolchain not found at ${LINARO_BIN}/arm-none-linux-gnueabi-gcc")
+# endif()
 
-# Clone device sysroot if not present
-if(NOT EXISTS "${DEVICE_SYSROOT}/.git")
-    message(STATUS "Device sysroot not found, cloning from ${DEVICE_SYSROOT_REPO}...")
-    execute_process(
-        COMMAND git clone --depth 1 ${DEVICE_SYSROOT_REPO} ${DEVICE_SYSROOT}
-        WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
-        RESULT_VARIABLE SYSROOT_CLONE_RESULT
-    )
-    if(NOT SYSROOT_CLONE_RESULT EQUAL 0)
-        message(FATAL_ERROR "Failed to clone device sysroot from ${DEVICE_SYSROOT_REPO}")
-    endif()
-endif()
+# # Clone device sysroot if not present
+# if(NOT EXISTS "${DEVICE_SYSROOT}/.git")
+#     message(STATUS "Device sysroot not found, cloning from ${DEVICE_SYSROOT_REPO}...")
+#     execute_process(
+#         COMMAND git clone --depth 1 ${DEVICE_SYSROOT_REPO} ${DEVICE_SYSROOT}
+#         WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+#         RESULT_VARIABLE SYSROOT_CLONE_RESULT
+#     )
+#     if(NOT SYSROOT_CLONE_RESULT EQUAL 0)
+#         message(FATAL_ERROR "Failed to clone device sysroot from ${DEVICE_SYSROOT_REPO}")
+#     endif()
+# endif()
 
 # Set compilers
-set(CMAKE_C_COMPILER ${LINARO_BIN}/arm-linux-gnueabi-gcc)
-set(CMAKE_CXX_COMPILER ${LINARO_BIN}/arm-linux-gnueabi-g++)
+set(CMAKE_C_COMPILER ${LINARO_BIN}/arm-linux-gnueabihf-gcc)
+set(CMAKE_CXX_COMPILER ${LINARO_BIN}/arm-linux-gnueabihf-g++)
 
 # Set sysroot for cross-compilation
 # Hybrid approach: Use Linaro for build-time files (crt*.o, headers)
 # but link against device libraries for runtime compatibility
-set(LINARO_SYSROOT "${LINARO_DIR}/arm-linux-gnueabi/libc")
+#set(LINARO_SYSROOT "${LINARO_DIR}/arm-linux-gnueabi/libc")
 
 # Use Linaro sysroot for build (provides crt*.o startup files)
 set(CMAKE_SYSROOT ${LINARO_SYSROOT})
-set(CMAKE_FIND_ROOT_PATH ${LINARO_SYSROOT})
+set(CMAKE_FIND_ROOT_PATH ${LINARO_SYSROOT};)
 
 # Add device sysroot to library search paths (takes precedence for runtime libs)
 set(CMAKE_LIBRARY_PATH 
+    "${LINARO_SYSROOT}/lib"
+    "${LINARO_SYSROOT}/usr/lib"
     "${DEVICE_SYSROOT}/lib"
     "${DEVICE_SYSROOT}/usr/lib"
-    "${CMAKE_SYSROOT}/lib"
-    "${CMAKE_SYSROOT}/usr/lib"
 )
 
 # Search for programs in the build host directories
@@ -82,13 +67,13 @@ set(CMAKE_CXX_DEPENDS_USE_COMPILER FALSE)
 set(CMAKE_C_DEPENDS_USE_COMPILER FALSE)
 
 # Set common ARM flags
-set(CMAKE_C_FLAGS_INIT "--sysroot=${CMAKE_SYSROOT} -march=armv7-a -mfloat-abi=soft")
-set(CMAKE_CXX_FLAGS_INIT "--sysroot=${CMAKE_SYSROOT} -march=armv7-a -mfloat-abi=soft")
+set(CMAKE_C_FLAGS_INIT "-march=armv7-a -mfloat-abi=hard")
+set(CMAKE_CXX_FLAGS_INIT "-march=armv7-a -mfloat-abi=hard")
 
-# Add linker flags: prioritize device libraries, use Linaro for startup files
-# -L flags are searched in order, so device libs are found first
-set(CMAKE_EXE_LINKER_FLAGS_INIT "--sysroot=${CMAKE_SYSROOT} -L${DEVICE_SYSROOT}/lib -L${DEVICE_SYSROOT}/usr/lib -Wl,-rpath-link,${DEVICE_SYSROOT}/lib:${DEVICE_SYSROOT}/usr/lib")
-set(CMAKE_SHARED_LINKER_FLAGS_INIT "--sysroot=${CMAKE_SYSROOT} -L${DEVICE_SYSROOT}/lib -L${DEVICE_SYSROOT}/usr/lib -Wl,-rpath-link,${DEVICE_SYSROOT}/lib:${DEVICE_SYSROOT}/usr/lib")
+# Add linker flags: explicitly link libstdc++ from device sysroot first
+# Link device libraries first, then fall back to Linaro for any missing symbols
+#set(CMAKE_EXE_LINKER_FLAGS_INIT "--sysroot=${CMAKE_SYSROOT} -L${DEVICE_SYSROOT}/usr/lib -L${DEVICE_SYSROOT}/lib -L${LINARO_SYSROOT}/usr/lib -Wl,-rpath-link,${DEVICE_SYSROOT}/lib:${DEVICE_SYSROOT}/usr/lib:${LINARO_SYSROOT}/lib")
+#set(CMAKE_SHARED_LINKER_FLAGS_INIT "--sysroot=${CMAKE_SYSROOT} -L${DEVICE_SYSROOT}/usr/lib -L${DEVICE_SYSROOT}/lib -L${LINARO_SYSROOT}/usr/lib -Wl,-rpath-link,${DEVICE_SYSROOT}/lib:${DEVICE_SYSROOT}/usr/lib:${LINARO_SYSROOT}/lib")
 
 # Enable C++11 support
 set(CMAKE_CXX_STANDARD 11)
